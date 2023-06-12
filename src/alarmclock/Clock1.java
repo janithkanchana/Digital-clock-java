a2fd752902b47cc694a47736c12ca8a23883fd65
ThaSha99 <sasi1999poorni@gmail.com>
2023-06-12 21:46:31 +0530

    method components

--- src/alarmclock/Clock1.java
+++ src/alarmclock/Clock1.java
@@ -22,4 +22,29 @@ public class Clock1 extends JFrame {
     public static final String path = "E:/_.Programming/Java/zzzzzz_____icons/beep_alarm.mp3";
     MP3Player mp3 = new MP3Player(new File(path));
 
+    //shashini start
+    public void initComponents() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setBounds(1270, 170, 560, 320);
+        this.setTitle("Alarm Clock");
+
+        c = this.getContentPane();
+        c.setLayout(null);
+        c.setBackground(Color.BLACK);
+
+        f1 = new Font("Arial", Font.BOLD, 20);
+        f2 = new Font("Digital-7 Mono", Font.BOLD, 46);
+        f3 = new Font("Digital-7", Font.PLAIN, 90);
+        f4 = new Font("Tahoma", Font.BOLD, 36);
+
+        icon = new ImageIcon(getClass().getResource("Miscellaneous-Icon.jpg"));
+        this.setIconImage(icon.getImage());
+
+        img = new ImageIcon(getClass().getResource("alarm.jpg"));
+        imgLabel = new JLabel(img);
+        imgLabel.setBounds(30, 215, img.getIconWidth(), 40);
+        c.add(imgLabel);
+        //shashini end
+    }
+    }
 }
